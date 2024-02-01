aaa472583c40ecd9ab4cc57e37e07cc91ec8a4ed
Faheem6765 <codingalt7@gmail.com>
2024-02-01 23:48:17 +0500

    message

--- src/components/Rejectpopup.jsx
+++ src/components/Rejectpopup.jsx
@@ -38,7 +38,7 @@ const Rejectpopup = ({ show, setShow }) => {
 
   return (
     <div>
-      <div className="overlay" style={{ overflow: "hidden",zIndex:999 }}>
+      <div className="overlay" style={{ overflow: "hidden", zIndex: 999 }}>
         <div className="Rejectpopoup" ref={buttonRef}>
           <img
             onClick={() => setShow(false)}
@@ -49,7 +49,12 @@ const Rejectpopup = ({ show, setShow }) => {
           />
           <p className="Confermation-text">Rejected</p>
           <p className="Reason-input" style={{ paddingLeft: "18px" }}>
-            {isLoading ? "Loading.." : data?.application?.rejectPhaseReason}
+            {isLoading
+              ? "Loading.."
+              : data?.application?.rejectPhaseReason
+              ? data?.application?.rejectPhaseReason
+              : data?.application?.phase3.reason
+              }
           </p>
 
           <button ref={buttonRef} className="Cncl-btn" onClick={handleCancel}>
